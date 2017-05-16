51d38afd9f79dc8d5ec17111b0664f5165def830
Jean-Claude Dufourd <dufourd@telecom-paristech.fr>
2017-05-16 16:28:13 +0200

    background processing

--- getinfofrompage.js
+++ getinfofrompage.js
@@ -7,36 +7,58 @@ function processHtml(html) {
   const { JSDOM } = jsdom;
   const document = new JSDOM(html).window.document;
   const scripts = document.getElementsByTagName('script');
+  const style = document.getElementsByTagName('style')[0].textContent;
+  const pageIndex = style.indexOf('.page {');
+  const endPageIndex = style.indexOf('}', pageIndex);
+  const backgroundColorIndex = style.indexOf('background-color:', pageIndex);
+  const backgroundImageIndex = style.indexOf('background-image:', pageIndex);
+  let background = '';
+  if (backgroundImageIndex > 0 && backgroundImageIndex < endPageIndex) {
+    background =
+      style.substring(backgroundImageIndex + 23,
+                      style.indexOf('"', backgroundImageIndex+24));
+  } else if (backgroundColorIndex > 0 && backgroundColorIndex < endPageIndex) {
+    background = style.substring(backgroundColorIndex + 18,
+                                 style.indexOf(';', backgroundColorIndex + 19));
+  }
   let script = null;
   for (let i = 0; i < scripts.length; i++) {
     script = scripts[i];
     if (script.textContent.indexOf('MPATGlobalInformation') > 0) break;
   }
+  if (script === null) {
+    console.log('did not find an MPAT script in this content');
+    console.log(scripts.length);
+    return;
+  }
   eval(script.textContent);
+  background = MPATGlobalInformation.Post.meta.background || background;
+  console.log(`background: ${background}`);
   const result = {
-    "page": {
-      "post_name": exportName,
-      "post_title": exportName,
-      "meta": {
-        "mpat_content": {
+    page: {
+      post_name: exportName,
+      post_title: exportName,
+      meta: {
+        mpat_content: {
           componentStyles: MPATGlobalInformation.Post.meta.componentStyles,
-          background: MPATGlobalInformation.Post.meta.background,
+          background: background,
           content: MPATGlobalInformation.Post.meta.content
         }
       }
     },
-    "page_layout": {
-      "post_name": exportName+'layout',
-      "post_title": exportName+'layout',
-      "meta": {
-        "mpat_content": {
+    page_layout: {
+      post_name: `${exportName}layout`,
+      post_title: `${exportName}layout`,
+      meta: {
+        mpat_content: {
           layout: MPATGlobalInformation.Post.meta.layout
         }
       }
     }
   };
-  fs.writeFileSync(exportName+".mpat-page",
+  fs.writeFileSync(`${exportName}.mpat-page`,
                 JSON.stringify(result));
+  console.log(`Wrote: ${exportName}.mpat-page`);
 }
 
 afterLoad(process.argv[2], processHtml);
